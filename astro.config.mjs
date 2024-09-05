7aba868677e157a4984c86f33cdf05a4156696a0
Jesus Perez <thejesuskor@gmail.com>
2024-09-05 01:48:44 -0600

    primera prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
+import vue from '@astrojs/vue';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind()],
+  output: 'server',
+  adapter: vercel()
+});
---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const buttonStatus = ref(true)
+
+const handleClick = () => {
+  buttonStatus.value = !buttonStatus.value
+  console.log('buttonStatus', buttonStatus.value)
+}
+
+</script>
+
+<template>
+  <div class="space-y-4">
+    <div @click="handleClick" class="bg-white p-4 rounded-2xl text-center font-semibold text-blue-400">Click me</div>
+    <div class="border border-white p-4 text-white font-semibold text-center rounded-2xl">
+      {{ buttonStatus }}
+    </div>
+  </div>
+</template>
